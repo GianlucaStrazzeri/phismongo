17f624b039ecb432adc21ffa457a4e0d3ba1bf6f
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-04-22 07:06:50 +0200

    modifyng name of directories

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');//requiero express
 const app = express();//inicializo express
 const PORT=process.env.PORT||3000;//defino el puerto de esta forma para que pueda luego exportarlo para la conexión base de datos
 
-const {dbConnection}= require("./src/config/config.js") //requiero connexión database 
+const {dbConnection}= require("./src/config/db.js") //requiero connexión database 
+
+
 const {router} =require ("./src/routes/mainRoutes.js")//requiero el router desde mainRoutes
 dbConnection() //invoco la función antes de cualquier middleware para que no se reconecte a cada request
-
+app.disable('x-powered-by');//practica de seguridad para que no recibir ataques en cabeceras de express
 app.use(express.json());//Sirve como un middleware para todas las rutas, only parses JSON and only looks at requests where the Content-Type header matches the type option
 app.use(express.urlencoded({extended:true})); //Sirve como un middleware para todas las rutas, only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option. This parser accepts only UTF-8 encoding of the body .
 //{extended:true} sirve para corregir el error body-parser deprecated undefined extended: provide extended option
